811ccf73c3481c9542de7edcf1d37d6159bd0381
Herbert Straub <herbert@linuxhacker.at>
2012-08-30 08:27:08 +0200

    - Add Copyright Headers - Add a TODO List

--- src/at/linuxhacker/battery/EventFilter.java
+++ src/at/linuxhacker/battery/EventFilter.java
@@ -1,11 +1,30 @@
+/*
+ * Copyright (C) 2012 Herbert Straub, herbert@linuxhacker.at
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package at.linuxhacker.battery;
 
 import android.content.Context;
 import at.linuxhacker.notifications.NotificationDistributor;
 
 public class EventFilter {
+	private static final long SPEAKER_SILENT_INTERVALL = 1800;
+	
 	private Context context = null;
 	private NotificationDistributor notificationDistributor = null;
+	long lastSpokenTimestamp = 0; // Unix Timestamp in Seconds
 
 	public EventFilter(Context context) {
 		this.context = context;
@@ -24,6 +43,7 @@ public class EventFilter {
 		}
 		this.notificationDistributor.displayTextOnScreenOverlay( text );
 		this.notificationDistributor.speakText( text );
+		this.markSpokenTimestamp( );
 	}
 	
 	public void processPowerPluggedOutEvent( BatteryStatusEvent batteryStatusEvent ) {
@@ -38,6 +58,7 @@ public class EventFilter {
 			this.notificationDistributor.speakText( text );
 			this.notificationDistributor.displayTextOnScreenOverlay( text );
 		}
+		this.markSpokenTimestamp( );
 	}
 	
 	public void processScreenOnEvent( BatteryStatusEvent batteryStatusEvent ) {
@@ -55,8 +76,25 @@ public class EventFilter {
 		 * drops all later one, until the first message process finished
 		 * 
 		 */
-		this.notificationDistributor.speakText( text );
+
+		if ( ! this.isSpeakerSilentInterval( ) ) {
+			this.notificationDistributor.speakText( text );
+			this.markSpokenTimestamp( );
+		}
 		this.notificationDistributor.displayTextOnScreenOverlay( text );
 	}
-		
+
+	private void markSpokenTimestamp( ) {
+		this.lastSpokenTimestamp = System.currentTimeMillis( ) / 1000;
+	}
+	
+	private boolean isSpeakerSilentInterval( ) {
+		if ( System.currentTimeMillis( ) / 1000
+				- this.lastSpokenTimestamp
+				> EventFilter.SPEAKER_SILENT_INTERVALL ) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 }
